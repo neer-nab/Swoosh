20f5ec9ea777862c94f3330e16391340a7f0a96a
neer-nab <neerajk207@gmail.com>
2018-02-06 23:01:14 +0530

    toggle button functionality and toast added

---
+++ app/src/main/java/com/example/neervyas/swoosh/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.neervyas.swoosh
+
+/**
+ * Created by neervyas on 6/2/18.
+ */
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/example/neervyas/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/neervyas/swoosh/LeagueActivity.kt
@@ -4,16 +4,59 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedleague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
-    fun leagueNextClicked(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+    fun onMenClicked(view: View){
+
+        womenLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedleague ="men"
+
     }
+
+    fun onWomenClicked(view: View){
+
+        menLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedleague = "women"
+
+    }
+
+    fun onCoedClicked(view: View){
+
+        menLeagueBtn.isChecked = false
+        womenLeagueBtn.isChecked = false
+
+        selectedleague = "co-ed"
+
+    }
+
+    fun leagueNextClicked(view: View) {
+
+
+
+        if (selectedleague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedleague)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+
 }
